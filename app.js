9dd1e7cf5f728af023b07f887eab5e8d0dbf1541
unknown <imbapalagz@gmail.com>
2021-05-19 10:16:08 +0800

    removed wiki from app.js

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/wiki', wiki);
 app.use('/catalog', catalogRouter);
 //app.use('/users/cool', usersCoolRouter);
 
